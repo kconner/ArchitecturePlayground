27bed8f27ce56228f19db4b1e43f32fbc9a0061f
Kevin Conner <connerk@gmail.com>
2023-04-05 19:43:54 -0700

    Initial Commit

---
+++ ArchitecturePlayground/ArchitecturePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitecturePlaygroundApp.swift
+//  ArchitecturePlayground
+//
+//  Created by Kevin Conner on 2023-04-05.
+//
+
+import SwiftUI
+
+@main
+struct ArchitecturePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
